2c0fb19e1d0e625f7a3c80925ac73ff73041afe0
willianVx <willianVoliveiravx@gmail.com>
2019-03-11 01:25:28 -0300

    incluindo menu desk

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import Search from './search/Search';
 import Menu from './menu/Menu';
 import Logo from '../utils/logo.js';
+import './style_header.css';
 
 class Header extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      switch_display: 'block'
+    }
+    this.handle_display = this.handle_display.bind(this);
+  }
+
+  handle_display(focus){
+    if (focus) {
+      this.setState({
+        switch_display:'none'
+      });
+    }else{
+      this.setState({
+        switch_display:'block'
+      });
+    }
+  }
+
   render(){
+    let switch_display = this.state.switch_display;
+
     return(
       <header>
-        <div>
-      	  <Menu />
-      	  <Logo />
-      	  <Search />
-      	</div>
+        <div className='header_container'>
+          <Menu display={switch_display}/>
+          <Logo display={switch_display} />
+          <Search whenPutInFocus={this.handle_display} />
+        </div>
       </header>
-	)
+    )
   }
 }
 
